8691779f13dc9c7723027a38da7fc215a510054b
Md Dilshad Akhtar <32808098+dilshadakhtar@users.noreply.github.com>
2025-03-08 08:41:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from streamlit_gsheets import GSheetsConnection
 st.title("Read Google Sheet as DataFrame")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Example 1")
+df = conn.read(worksheet="Sheet1")
 
 st.dataframe(df)
