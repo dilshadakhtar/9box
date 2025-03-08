8f465bd319459c61c7aa2cd21003a0ed9b150994
Md Dilshad Akhtar <32808098+dilshadakhtar@users.noreply.github.com>
2025-03-08 09:07:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from streamlit_gsheets import GSheetsConnection
 st.title("Read Google Sheet as DataFrame")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read()
+df = conn.read(
+    worksheet="Sheet1",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=3,
+)
 
 # Print results.
 for row in df.itertuples():
